57f4ad27694602999812d7e6a48b235ecf29c3f3
hyochan <dooboolab@gmail.com>
2018-06-15 09:50:44 +0900

    setup react-navigation in RNInit

---
+++ RNInit/src/navigations/RootStackNavigator.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { createStackNavigator } from 'react-navigation';
+import NavigationService from './NavigationService';
+import HomeScreen from '../screen/Home';
+
+class RootNavigator extends React.Component {
+  render() {
+    const RootStackNavigator = createStackNavigator({
+      Home: {
+        screen: HomeScreen,
+      },
+    });
+    return (
+      <RootStackNavigator
+        ref={(v) => {
+          if (v) {
+            NavigationService.setTopLevelNavigator(v);
+          }
+        }}
+        onNavigationStateChange={(prevState, currentState) => {
+          this.getActiveRouteName(currentState);
+        }}
+      />
+    )
+  }
+}
+
+export default createStackNavigator({
+  
+});
